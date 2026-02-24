b3f7b012100898d696d1a9b325565e540b45d4b7
Abinash Bhutia <abinashbhutia78@gmail.com>
2026-02-24 12:58:20 +0530

    second commit

--- Mydemo.java
+++ Mydemo.java
@@ -1,6 +1,9 @@
 class Mydemo{
     public static void main(String[] args){
         System.out.println("hello");
-    }
-    
+        System.out.println("hello world");
+        // System.out.println("hello");
+        System.out.println("Good Morning");
+        
+    }   
 }
